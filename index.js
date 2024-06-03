4adbe5f2da677804e7b4aa22a00a765e71ecff2b
Vulpes231 <whykayshot@gmail.com>
2024-06-03 01:31:25 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({ origin: true }));
+
+app.listen(PORT, () =>
+  console.log(`Server started on http://localhost:${PORT}`)
+);
