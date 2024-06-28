d5546e7bf5b07e93007dc729634b11d712352d25
Vulpes231 <whykayshot@gmail.com>
2024-06-28 01:49:46 +0100

    back

--- configs/corsOption.js
+++ configs/corsOption.js
@@ -1,4 +1,8 @@
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3300"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3300",
+  "https://quadx.io, https://admin.quadx.io, https://server.quadx.io",
+];
 
 const corsOptions = {
   origin: function (origin, callback) {
